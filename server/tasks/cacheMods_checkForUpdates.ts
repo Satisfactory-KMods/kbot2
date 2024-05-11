baf3ff7aead693c26738a632096b55a7f48acc2e
Oliver Kaufmann <olli143@live.de>
2024-05-11 07:09:59 +0200

    Update cacheMods_checkForUpdates.ts

--- server/tasks/cacheMods_checkForUpdates.ts
+++ server/tasks/cacheMods_checkForUpdates.ts
@@ -132,10 +132,7 @@ export async function checkForModUpdates() {
 						return;
 					}
 
-					console.log
-				!!exists ,
-				!!mod.last_version ,
-				semverGt(mod.last_version.version, exists.version),mod,mod.last_version.version, exists.version)
+					console.log(!!exists, !!mod.last_version, semverGt(mod.last_version.version, exists.version),mod,mod.last_version.version, exists.version);
 
 					const guild = await DiscordGuildManager.getGuild(guild_id);
 					if (guild.isValid()) {
